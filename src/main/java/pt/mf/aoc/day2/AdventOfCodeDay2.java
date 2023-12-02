1b86387cfe9107c75b88d83210979939409b2985
marcos-ferreira <marcos.7686@gmail.com>
2023-12-02 12:00:09 +0000

    completed part one

--- src/main/java/pt/mf/aoc/day2/AdventOfCodeDay2.java
+++ src/main/java/pt/mf/aoc/day2/AdventOfCodeDay2.java
@@ -20,17 +20,18 @@ public class AdventOfCodeDay2 extends AbstractAoc {
     private static final Pattern GAME_REGEX = Pattern.compile("Game (\\d*): (.*)");
     private static final Pattern GRAB_REGEX = Pattern.compile("(\\d*) (blue|red|green)");
 
+    private static final int MAX_RED_CUBES = 12;
+    private static final int MAX_GREEN_CUBES = 13;
+    private static final int MAX_BLUE_CUBES = 14;
+
     public static void main(String[] args) throws Exception {
         new AdventOfCodeDay2().run();
 
-        for (Map.Entry<Integer, List<Grab>> entry : gameMap.entrySet()) {
-            System.out.println(entry.getKey());
-            for (Grab grab : entry.getValue()) {
-                System.out.println(grab);
-            }
-        }
-    }
+        // print game map
+        printGameMap();
 
+        System.out.println("total possible games: " + processPossibleGames());
+    }
 
     @Override
     public String getFileResourceName() {
@@ -53,27 +54,72 @@ public class AdventOfCodeDay2 extends AbstractAoc {
         }
     }
 
-    private Grab getGrab(String cubes) {
-        Matcher matcher = GRAB_REGEX.matcher(cubes);
+    private Grab getGrab(String grab) {
+        Matcher matcher = GRAB_REGEX.matcher(grab);
 
         var grabList = new ArrayList<Grab>();
-        Grab grab = new Grab();
+        var g = new Grab();
         while (matcher.find()) {
             var cubeCount = Integer.parseInt(matcher.group(1));
             var cube = getCube(matcher.group(2));
 
-            grab.getCubes().put(cube, cubeCount);
-            grabList.add(grab);
+            g.getCubes().put(cube, cubeCount);
+            grabList.add(g);
         }
-        return grab;
+        return g;
     }
 
-    private Cube getCube(String cubeColor) {
-        return switch (cubeColor) {
+    private Cube getCube(String cube) {
+        return switch (cube) {
             case "blue" -> Cube.BLUE;
             case "red" -> Cube.RED;
             case "green" -> Cube.GREEN;
-            default -> throw new IllegalArgumentException("Invalid cube color " + cubeColor);
+            default -> throw new IllegalArgumentException("Invalid cube color " + cube);
         };
+    }
+
+    private static int processPossibleGames() {
+        int total = 0;
+        for (Map.Entry<Integer, List<Grab>> entry : gameMap.entrySet()) {
+            Integer gameId = entry.getKey();
+
+            if (isPossibleGame(entry)) {
+                total += gameId;
+            }
+        }
+
+        return total;
+    }
+
+    private static boolean isPossibleGame(Map.Entry<Integer, List<Grab>> entry) {
+        for (Grab grab : entry.getValue()) {
+            Map<Cube, Integer> cubes = grab.getCubes();
+            for (Map.Entry<Cube, Integer> cube : cubes.entrySet()) {
+                Cube key = cube.getKey();
+                Integer value = cube.getValue();
+                if(!isValidGrab(key, value)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isValidGrab(Cube cube, Integer cubeCount) {
+        return switch (cube) {
+            case RED -> cubeCount <= MAX_RED_CUBES;
+            case BLUE -> cubeCount <= MAX_BLUE_CUBES;
+            case GREEN -> cubeCount <= MAX_GREEN_CUBES;
+        };
+    }
+
+    private static void printGameMap() {
+        for (Map.Entry<Integer, List<Grab>> entry : gameMap.entrySet()) {
+            System.out.println(entry.getKey());
+            for (Grab grab : entry.getValue()) {
+                System.out.println(grab);
+            }
+        }
     }
 }
